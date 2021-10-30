a7bd49e43219bec30d81c79c0ee8c57d3f6d37f6
reszi123 <imrezz07@gmail.com>
2021-10-30 17:52:23 +0300

    fix new slider

--- js/main.js
+++ js/main.js
@@ -7,15 +7,6 @@
 		once: true
 	});
 
-	const swiper = new Swiper('.swiper', {
-		direction: 'vertical',
-		loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-	});
-
 	var slider = function () {
 
 		var carouselSlider = document.querySelectorAll('.carousel-testimony');
