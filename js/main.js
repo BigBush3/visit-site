ebd23c6f9dd18e3a1a879211acb5dded3fab7ef4
reszi123 <imrezz07@gmail.com>
2021-10-31 14:34:08 +0300

    complete site

--- js/main.js
+++ js/main.js
@@ -1,116 +1,116 @@
 (function () {
 
-	"use strict";
+    "use strict";
 
-	AOS.init({
-		ease: 'slide',
-		once: true
-	});
+    AOS.init({
+        ease: 'slide',
+        once: true
+    });
 
-	var slider = function () {
+    var slider = function () {
 
-		var carouselSlider = document.querySelectorAll('.carousel-testimony');
+        var carouselSlider = document.querySelectorAll('.carousel-testimony');
 
-		if (carouselSlider.length > 0) {
+        if (carouselSlider.length > 0) {
 
-			var testimonySlider = tns({
-				container: '.carousel-testimony',
-				items: 1,
-				mode: 'carousel',
-				autoplay: true,
-				animateIn: 'tns-fadeIn',
-				animateOut: 'tns-fadeOut',
-				speed: 700,
-				nav: false,
-				gutter: 20,
-				controls: true,
-				controlsText: ["<span class='iconify' data-icon='ion:chevron-back-outline'></span>", "<span class='iconify' data-icon='ion:chevron-forward-outline'></span>"],
-				autoplayButtonOutput: false,
-				responsive: {
-					0: {
-						items: 1,
-						gutter: 0
-					},
-					600: {
-						items: 1,
-						gutter: 20
-					},
-					1000: {
-						items: 1,
-						gutter: 20
-					}
-				}
-			});
-		}
-	}
-	slider();
+            var testimonySlider = tns({
+                container: '.carousel-testimony',
+                items: 1,
+                mode: 'carousel',
+                autoplay: true,
+                animateIn: 'tns-fadeIn',
+                animateOut: 'tns-fadeOut',
+                speed: 700,
+                nav: false,
+                gutter: 20,
+                controls: true,
+                controlsText: ["<span class='iconify' data-icon='ion:chevron-back-outline'></span>", "<span class='iconify' data-icon='ion:chevron-forward-outline'></span>"],
+                autoplayButtonOutput: false,
+                responsive: {
+                    0: {
+                        items: 1,
+                        gutter: 0
+                    },
+                    600: {
+                        items: 1,
+                        gutter: 20
+                    },
+                    1000: {
+                        items: 1,
+                        gutter: 20
+                    }
+                }
+            });
+        }
+    }
+    slider();
 
-	var lang = document.getElementById('lang');
-	var path = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
-	lang.onchange = function () {
-		window.location.pathname = ''
-		window.location.pathname = `visit-site${lang.options[lang.options.selectedIndex].value}${path}`
-	}
+    var lang = document.getElementById('lang');
+    var path = location.pathname.substring(location.pathname.lastIndexOf("/") + 1);
+    lang.onchange = function () {
+        window.location.pathname = ''
+        window.location.pathname = `visit-site${lang.options[lang.options.selectedIndex].value}${path}`
+    }
 
-	var counter = function () {
-		function countUp(elem) {
-			var current = elem.innerHTML;
+    var counter = function () {
+        function countUp(elem) {
+            var current = elem.innerHTML;
 
-			var timeIntervalBeforeIncrement = 2000 / elem.getAttribute("data-count")
+            var timeIntervalBeforeIncrement = 2000 / elem.getAttribute("data-count")
 
-			var interval = setInterval(increase, timeIntervalBeforeIncrement);
+            var interval = setInterval(increase, timeIntervalBeforeIncrement);
 
-			function increase() {
-				elem.innerHTML = current++;
-				if (current > elem.getAttribute("data-count")) {
-					clearInterval(interval);
-				}
-			}
+            function increase() {
+                elem.innerHTML = current++;
+                if (current > elem.getAttribute("data-count")) {
+                    clearInterval(interval);
+                }
+            }
 
-		}
-		var span = document.querySelectorAll("[id^='count']");
+        }
+        var span = document.querySelectorAll("[id^='count']");
 
-		var i = 0;
-		for (i = 0; i < span.length; i++) {
-			countUp(span[i]);
-		}
-	}
+        var i = 0;
+        for (i = 0; i < span.length; i++) {
+            countUp(span[i]);
+        }
+    }
 
-	var elements;
-	var windowHeight;
+    var elements;
+    var windowHeight;
 
-	function init() {
-		elements = document.querySelectorAll('.ftco-about-section');
-		windowHeight = window.innerHeight;
-	}
+    function init() {
+        elements = document.querySelectorAll('.ftco-about-section');
+        windowHeight = window.innerHeight;
+    }
 
-	function checkPosition() {
-		var i;
-		for (i = 0; i < elements.length; i++) {
-			var element = elements[i];
-			var positionFromTop = elements[i].getBoundingClientRect().top;
-			if (positionFromTop - windowHeight <= 0) {
-				if (!element.classList.contains('viewed')) {
-					element.classList.add('viewed');
-					counter();
-				} else {
-					if (element.classList.contains('viewed')) {
+    function checkPosition() {
+        var i;
+        for (i = 0; i < elements.length; i++) {
+            var element = elements[i];
+            var positionFromTop = elements[i].getBoundingClientRect().top;
+            if (positionFromTop - windowHeight <= 0) {
+                if (!element.classList.contains('viewed')) {
+                    element.classList.add('viewed');
+                    counter();
+                } else {
+                    if (element.classList.contains('viewed')) {
 
-					}
-				}
-			}
-		}
-	}
-	window.addEventListener('scroll', checkPosition);
-	window.addEventListener('resize', init);
+                    }
+                }
+            }
+        }
+    }
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
 
-	init();
-	checkPosition()
+    init();
+    checkPosition()
 
-	const lightbox = GLightbox({
-		touchNavigation: true,
-		loop: true,
-		autoplayVideos: true
-	});
+    const lightbox = GLightbox({
+        touchNavigation: true,
+        loop: true,
+        autoplayVideos: true
+    });
 })()
 
